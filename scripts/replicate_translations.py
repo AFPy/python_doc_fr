214cb6e6993c9bf3c1b8163676f0799a4d373098
Julien Palard <julien@palard.fr>
2015-12-19 18:47:00 +0100

    Script to replicate translation between versions.

---
+++ scripts/replicate_translations.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+import polib
+from collections import defaultdict
+
+
+def group_by_filename(po_files):
+    """
+    For each po file in */*.po, aggregate them by directory name, like:
+
+    >>> group_by_filename(['2.7/foo.po', '2.7/bar.po', '3.2/foo.po'])
+    [['2.7/bar.po'], ['2.7/foo.po', '3.2/foo.po']]
+    """
+    po_per_version = defaultdict(list)
+    for po_file in po_files:
+        po_per_version[os.path.basename(po_file)].append(po_file)
+    return sorted(po_per_version.values())
+
+
+
+def merge_po_file(po_files):
+    known_translations = {}
+    # Aggregate all known translations
+    for po_file in po_files:
+        po_file = polib.pofile(po_file)
+        for entry in po_file:
+            if entry.msgid not in known_translations:
+                known_translations[entry.msgid] = entry.msgstr
+    # Propagate them
+    for po_file in po_files:
+        po_file = polib.pofile(po_file)
+        for entry in po_file:
+            if entry.msgstr == '' and entry.msgid in known_translations:
+                entry.msgstr = known_translations[entry.msgid]
+        po_file.save()
+
+
+def merge_po_files():
+    for po_files in group_by_filename(glob.glob('*/*.po')):
+        merge_po_file(po_files)
+
+if __name__ == '__main__':
+    merge_po_files()
