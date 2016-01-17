311a1ca5d054f44398eada12f5f3ecb39aa4e8c1
Julien Palard <julien@palard.fr>
2016-01-17 17:03:01 +0100

    fix_style: Fix only uncommited by default.

--- scripts/fix_style.sh
+++ scripts/fix_style.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-for po in */*.po
-do
-    echo "$po"
-    tac "$po" | tac | msgcat - -o "$po"
-done
+# Fix style of uncommited po files, or all if --all is given.
+
+if [ z"$1" = z"--all" ]
+then
+    for po in */*.po
+    do
+        echo "$po"
+        tac "$po" | tac | msgcat - -o "$po"
+    done
+else
+    for po in $(git status | grep 'modified.*\.po$' | rev | cut -d' ' -f1 | rev)
+    do
+        echo "$po"
+        tac "$po" | tac | msgcat - -o "$po"
+    done
+fi
