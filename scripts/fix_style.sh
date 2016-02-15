a87aaee88340159509f8390198d4209111c6ae02
Julien Palard <julien@palard.fr>
2016-02-15 14:11:26 +0100

    Document the tac|tac trick.

--- scripts/fix_style.sh
+++ scripts/fix_style.sh
@@ -2,6 +2,13 @@
 
 # Fix style of uncommited po files, or all if --all is given.
 
+# The "tac|tac" trick is an equivalent to "sponge" but as tac is in
+# coreutils we're avoiding you to install a new packet.  Without
+# `tac|tac` or `sponge`, `msgcat` may start to write in the po file
+# before having finised to read it, yielding unpredictible behavior.
+# (Yep we also can write to another file and mv it, like sed -i does.)
+
+
 if [ z"$1" = z"--all" ]
 then
     for po in */*.po
