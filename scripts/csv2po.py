8a5960fc0fadcd9c9e46f3aa5fa5ec1c2607f5ed
Julien Palard <julien@palard.fr>
2016-03-16 09:45:17 +0100

    Add mangling/unmangling function to csv2po/po2csv to avoid google translate fscking with sphinx tags.

--- scripts/csv2po.py
+++ scripts/csv2po.py
@@ -4,14 +4,35 @@ import csv
 import polib
 
 
-def csv2po(csvfile, pofile, msgid_column=0, msgstr_column=1):
+def unmangle_string(string):
+    """unmangle rest syntax like :foo:`bar` and *x* so an automatic
+    translator won't fsck with them.
+
+    Mangling syntax is easy, it's a base32 encoded, UTF-8, sphinx tag
+    prefixed by 'SPX'.
+    """
+    import re
+    from base64 import b32decode
+
+    def repl(match):
+        found = match.group(0)[3:].replace('i', '=')
+        return b32decode(found).decode('utf-8')
+    return re.sub('SPX[A-Z0-9i]+', repl, string)
+
+
+def csv2po(csvfile, pofile, msgid_column=0, msgstr_column=1, unmangle=False):
+    if unmangle:
+        unmangler = unmangle_string
+    else:
+        unmangler = lambda x: x
     translations = {}
     with open(csvfile) as opened_csvfile:
         dialect = csv.Sniffer().sniff(opened_csvfile.read())
         opened_csvfile.seek(0)
         reader = csv.reader(opened_csvfile, dialect)
         for line in reader:
-            translations[line[msgid_column]] = line[msgstr_column]
+            translations[unmangler(line[msgid_column])] = unmangler(
+                line[msgstr_column])
     po = polib.pofile(pofile)
     for entry in po:
         if entry.msgstr == "" and entry.msgid in translations:
@@ -28,6 +49,8 @@ def parse_args():
     parser.add_argument('pofile')
     parser.add_argument('--msgid-column', default=0)
     parser.add_argument('--msgstr-column', default=1)
+    parser.add_argument('--unmangle', action='store_true',
+                        help='Unmangle previously mangled sphinx tags.')
     return parser.parse_args()
 
 if __name__ == '__main__':
--- scripts/po2csv.py
+++ scripts/po2csv.py
@@ -4,13 +4,42 @@ import csv
 import polib
 
 
-def po2csv(csvfile, pofile, untranslated_only=False):
+def mangle_string(string):
+    """mangle rest syntax like :foo:`bar` and *x* so an automatic
+    translator won't fsck with them.
+
+    Mangling syntax is easy, it's a base32 encoded, UTF-8, sphinx tag
+    prefixed by 'SPX'.
+    """
+    import re
+    from base64 import b32encode
+
+    def repl(match):
+        return 'SPX' + b32encode(
+            match.group(0).encode('utf-8')).decode().replace('=', 'i')
+    sphinx_structs = [r':[a-zA-Z:]+`[^`]+`',
+                      r'``[^`]+``',
+                      r'\*\*[^*]+\*\*',
+                      r'`[^`]+`',
+                      r'\*[^* ]+\*']
+    for struct in sphinx_structs:
+        string = re.sub(struct, repl, string)
+    return string
+
+
+def po2csv(csvfile, pofile, untranslated_only=False, mangle=False):
     po = polib.pofile(pofile)
+    if mangle:
+        mangle_function = mangle_string
+    else:
+        mangle_function = lambda x: x
     with open(csvfile, 'w', newline='') as opened_csvfile:
         csvwriter = csv.writer(opened_csvfile)
         for entry in po:
-            if untranslated_only == False or entry.msgstr == '':
-                csvwriter.writerow([entry.msgid, entry.msgstr])
+            if untranslated_only is False or entry.msgstr == '':
+                csvwriter.writerow(
+                    [mangle_function(entry.msgid),
+                     entry.msgstr])
 
 
 def parse_args():
@@ -19,6 +48,8 @@ def parse_args():
         description="Create a CSV file from a PO file.")
     parser.add_argument('pofile')
     parser.add_argument('csvfile')
+    parser.add_argument('--mangle', help="Mangle rest syntax so an automatic "
+                        "translator won't fsck with them", action='store_true')
     parser.add_argument('--untranslated-only', action='store_true')
     return parser.parse_args()
 
