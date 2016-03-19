c8912b0f91d205266fd3b2f1c6a30fd0382a3a19
Julien Palard <julien@palard.fr>
2016-03-19 22:25:44 +0100

    FIX: variable is far too ambiguous to be used as a translation for mutable.

--- scripts/fix_style.py
+++ scripts/fix_style.py
@@ -15,14 +15,23 @@ from shlex import quote
 from subprocess import check_output
 
 
-def fix_style(all_files=False):
+def fix_style(all_files=False, no_wrap=False):
     find_modified_files = "git status | grep 'modified.*\.po$' | rev | cut -d' ' -f1 | rev"
     for po in tqdm(glob('*/*.po') if all_files else
                    check_output(find_modified_files, shell=True).decode().split(),
                    desc="Fixing indentation in po files"):
-        check_output('tac {} | tac | msgcat - -o {}'.format(
-            quote(po), quote(po)), shell=True)
+        check_output('tac {} | tac | msgcat - -o {} {}'.format(
+            quote(po), quote(po), '--no-wrap' if no_wrap else ''),
+                     shell=True)
 
 if __name__ == '__main__':
     import sys
-    fix_style('--all' in sys.argv)
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Ensure po files are using the standard gettext format')
+    parser.add_argument('--all', '-a', action='store_true',
+                        help='Recheck all files, not only modified ones.')
+    parser.add_argument('--no-wrap', action='store_true',
+                        help='see `man msgcat`, usefull for sed.')
+    args = parser.parse_args()
+    fix_style(args.all, args.no_wrap)
