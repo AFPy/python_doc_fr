8ead6a6780f14614924dd2d5554a67871272efb2
Julien Palard <julien@palard.fr>
2016-07-26 23:50:48 +0200

    tools: Hardening and testing a bit fixpo.py.

--- scripts/fixpo.py
+++ scripts/fixpo.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
 import re
+import sys
 import polib
 
 
 def fix_spaces_inside_quotes(text, quote='``'):
+    """
+    >>> test = '''\
+    :meth:`update` accepte soit un autre objet dictionnaire ou un iterable de\
+    paires clé / valeur (comme tuples ou d'autres iterables de longueur deux).\
+    Si les arguments de mots clés sont spécifiés, le dictionnaire est alors mis\
+    à jour avec ces paires clé / valeur: ``d.update(red=1, blue=2)``.\
+    '''
+    >>> fixed = fix_spaces_inside_quotes(test, '``')
+    >>> ':meth:`update`' in fixed
+    True
+    >>> fixed = fix_spaces_inside_quotes(test, '*')
+    >>> ':meth:`update`' in fixed
+    True
+    """
     chunks = text.split(quote)
     is_inside_quote = False
     for i, chunk in enumerate(chunks):
@@ -15,16 +30,29 @@ def fix_spaces_inside_quotes(text, quote='``'):
 
 
 def fix_reference(text):
+    """
+    >>> fix_reference(":ref:`test`")
+    ':ref:`test`'
+    >>> fix_reference(":ref: `test`")
+    ':ref:`test`'
+    >>> fix_reference("   :meth:`update`   ")
+    '   :meth:`update`   '
+    >>> fix_reference("   :meth:`update`, et: ``foo``")
+    '   :meth:`update`, et: ``foo``'
+    >>> fix_reference(":meth:`update` : ``d.update(red=1, blue=2)``.")
+    ':meth:`update` : ``d.update(red=1, blue=2)``.'
+    """
     def repl(match):
-        has_left_space = match.group(1) == ' '
+        suffix = ' ' if match.group(1) != ' ' and match.group(1) != '' else ''
         return '{}:{}:`{}`'.format(
-            match.group(1) if has_left_space else match.group(1) + ' ',
+            match.group(1) + suffix,
             match.group(2).strip(),
             match.group(3).strip())
-    return re.sub('(.):([^:]*):\s*`([^`]*)`', repl, text)
+    find = r'(.|^):([^:\`]*):\s*`([^`]*)`'
+    return re.sub(find, repl, text)
 
 
-def fixpo(pofile, only_fuzzy=False):
+def fixpo(pofile, only_fuzzy=False, doctest=None):
     po = polib.pofile(pofile)
     for entry in po:
         if only_fuzzy and 'fuzzy' not in entry.flags:
@@ -33,7 +61,11 @@ def fixpo(pofile, only_fuzzy=False):
             continue
         text = fix_spaces_inside_quotes(entry.msgstr, '``')
         text = fix_spaces_inside_quotes(text, '*')
-        entry.msgstr = fix_reference(text)
+        text = fix_reference(text)
+        if ':meth:`update`' in entry.msgstr:
+            if ':meth:`update`' not in text:
+                print(entry, repr(fix_bad_translations(fix_spaces_inside_quotes(fix_spaces_inside_quotes(entry.msgstr, '``'), '*'))))
+        entry.msgstr = text
     po.save()
 
 
@@ -42,8 +74,13 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description="Fix Google Translate fsckeries in a po file.")
     parser.add_argument('pofile')
+    parser.add_argument('--doctest')
     parser.add_argument('--only-fuzzy', action='store_true')
     return parser.parse_args()
 
 if __name__ == '__main__':
+    if '--doctest' in sys.argv:
+        import doctest
+        doctest.testmod()
+        exit(0)
     fixpo(**vars(parse_args()))
